d75b4a537148bc42c5d96bcdffbe8c3f35f54896
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-12 14:00:27 -0800

    Added button and button selector action

--- PhotoFilterPrototype/ViewController.swift
+++ PhotoFilterPrototype/ViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  
+  let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: UIAlertControllerStyle.ActionSheet)
+
   override func loadView() {
     let rootView = UIView(frame: UIScreen.mainScreen().bounds)
     rootView.backgroundColor = UIColor.whiteColor()
-    
-    
-    
-    
-    
-    
+    let photoButton = UIButton()
+    photoButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+    rootView.addSubview(photoButton)
+    photoButton.setTitle("My Photos", forState: .Normal)
+    photoButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+    photoButton.addTarget(self, action: "photoButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+    let views = ["photoButton" : photoButton]
+    self.setupConstraintsOnRootView(rootView, forViews: views)
+
     self.view = rootView
-    
-    
-    
-    
+  }
+  
+  //MARK: Button Selectors
+  
+  func photoButtonPressed(sender : UIButton) {
+    self.presentViewController(self.alertController, animated: true, completion: nil)
   }
 
   override func viewDidLoad() {
@@ -35,6 +41,15 @@ class ViewController: UIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  //MARK: Autolayout Constraints
+  func setupConstraintsOnRootView(rootView : UIView, forViews views : [String : AnyObject]) {
+    let photoButtonConstraintVertical = NSLayoutConstraint.constraintsWithVisualFormat("V:[photoButton]-20-|", options: nil, metrics: nil, views: views)
+    rootView.addConstraints(photoButtonConstraintVertical)
+    let photoButton = views["photoButton"] as UIView!
+    let photoButtonConstraintHorizontal = NSLayoutConstraint(item: photoButton, attribute: .CenterX, relatedBy: NSLayoutRelation.Equal, toItem: rootView, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0)
+    rootView.addConstraint(photoButtonConstraintHorizontal)
   }
 
 
