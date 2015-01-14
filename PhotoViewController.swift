32dcd8af0cc62099fcd9528e282eeef2ec15af0b
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-14 13:35:19 -0800

    Added PhotoViewController.swift

---
+++ PhotoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoViewController.swift
+//  PhotoFilterPrototype
+//
+//  Created by Rodrigo Carballo on 1/14/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+import UIKit
+import Photos
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      
+      
+      
+      
+      
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+}
